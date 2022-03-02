af4d539c7dfc4b21821a9330fb9da9d732298f5e
HP <HP@LAPTOP-QRLO59E6>
2022-03-02 14:12:26 +0530

    1st commit

--- src/selenium/demo.java
+++ src/selenium/demo.java
@@ -1,10 +1,10 @@
 package selenium;
 
-public class demo {
+public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+      System.out.println("hi from local");
 	}
 
 }
